3f39821189bddeb9fa9cd96fea7b6d0a4b61d2a5
Alex <asanc305@fiu.edu>
2017-02-14 20:37:39 -0500

    Toast Test

--- app/src/main/java/com/example/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -1,10 +1,12 @@
 package com.example.myfirstapp;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,6 +23,13 @@ public class MainActivity extends AppCompatActivity {
     /** Called when the user clicks the Send button */
     public void sendMessage(View view) {
 
+        String x = Integer.toString(view.getId());
+
+        Context context = getApplicationContext();
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, x, duration);
+        toast.show();
     }
 
     public void numberPressed(View view) {
